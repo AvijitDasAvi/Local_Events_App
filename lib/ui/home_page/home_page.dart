e94ba5d1b9febfb376771018f055c3bccbb319af
Avijit Das <avijitavi338895@gmail.com>
2024-05-29 18:04:11 +0600

    Created Home Page Background

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_events_app/ui/home_page/home_page_background.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
+        children: <Widget>[
           HomePageBackground(),
         ],
       ),
---
+++ lib/ui/home_page/home_page_background.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePageBackground extends StatefulWidget {
+  const HomePageBackground({super.key});
+
+  @override
+  State<HomePageBackground> createState() => _HomePageBackgroundState();
+}
+
+class _HomePageBackgroundState extends State<HomePageBackground> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
