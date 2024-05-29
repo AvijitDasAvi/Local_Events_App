90494fd79143fdc2065b8cbd7d8d5a0c762c4b3b
Avijit Das <avijitavi338895@gmail.com>
2024-05-29 18:56:04 +0600

    Home Background Complete

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:local_events_app/ui/home_page/home_page_background.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          HomePageBackground(),
+          HomePageBackground(
+            screenHeight: MediaQuery.of(context).size.height,
+          ),
         ],
       ),
     );
