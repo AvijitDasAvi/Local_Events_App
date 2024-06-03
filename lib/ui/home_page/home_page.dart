b36524ed2cadd6976f4aba7dd2d273aec2653f8d
Avijit Das <avijitavi338895@gmail.com>
2024-06-04 01:06:08 +0600

    Home Page Updated

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_events_app/styleguide.dart';
 import 'package:local_events_app/ui/home_page/home_page_background.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,10 +9,48 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: <Widget>[
+        children: [
           HomePageBackground(
             screenHeight: MediaQuery.of(context).size.height,
           ),
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 30.0,
+                      right: 30.0,
+                      top: 10.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "LOCAL EVENTS",
+                          style: fadedTextStyle,
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.person_outline,
+                          color: Color(0x99FFFFFF),
+                          size: 30.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 30.0),
+                    child: Text(
+                      "What's Up",
+                      style: whiteHeadingTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
