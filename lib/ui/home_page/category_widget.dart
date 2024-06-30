f99d74ffeb01131b95bac797640c68c47af02339
Avijit Das <avijitavi338895@gmail.com>
2024-06-30 21:36:26 +0600

    Create App State and Update Home Page

--- lib/ui/home_page/category_widget.dart
+++ lib/ui/home_page/category_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:local_events_app/model/category.dart';
 import 'package:local_events_app/styleguide.dart';
+import 'package:local_events_app/ui/app_state.dart';
+import 'package:provider/provider.dart';
 
 class CategoryWidget extends StatelessWidget {
   final Category category;
@@ -8,29 +10,43 @@ class CategoryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 90,
-      height: 90,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white),
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-        color: Colors.transparent,
-      ),
-      child: Column(
-        children: [
-          Icon(
-            category.icon,
-            color: Theme.of(context).primaryColor,
-            size: 40,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            category.name,
-            style: categoryTextStyle,
-          )
-        ],
+    final appState = Provider.of<AppState>(context);
+    final isSelected = appState.selectedCategoryId == category.categoryId;
+    return GestureDetector(
+      onTap: () {
+        if (!isSelected) {
+          appState.updateCategoryId(category.categoryId);
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10.0),
+        width: 90,
+        height: 90,
+        decoration: BoxDecoration(
+          border: Border.all(
+              color: isSelected ? Colors.white : const Color(0x99FFFFFF),
+              width: 3.0),
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          color: isSelected ? Colors.white : Colors.transparent,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              category.icon,
+              color: isSelected ? Theme.of(context).primaryColor : Colors.white,
+              size: 40,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              category.name,
+              style: isSelected ? selectedCategoryTextStyle : categoryTextStyle,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:local_events_app/model/category.dart';
 import 'package:local_events_app/styleguide.dart';
+import 'package:local_events_app/ui/app_state.dart';
 import 'package:local_events_app/ui/home_page/category_widget.dart';
 import 'package:local_events_app/ui/home_page/home_page_background.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,59 +12,68 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          HomePageBackground(
-            screenHeight: MediaQuery.of(context).size.height,
-          ),
-          SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 30.0,
-                      right: 30.0,
-                      top: 10.0,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "LOCAL EVENTS",
-                          style: fadedTextStyle,
-                        ),
-                        Spacer(),
-                        Icon(
-                          Icons.person_outline,
-                          color: Color(0x99FFFFFF),
-                          size: 30.0,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 30.0),
-                    child: Text(
-                      "What's Up",
-                      style: whiteHeadingTextStyle,
-                    ),
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        for (final category in categories)
-                          CategoryWidget(category: category)
-                      ],
-                    ),
-                  )
-                ],
-              ),
+      body: ChangeNotifierProvider(
+        create: (_) => AppState(),
+        child: Stack(
+          children: [
+            HomePageBackground(
+              screenHeight: MediaQuery.of(context).size.height,
             ),
-          )
-        ],
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 30.0,
+                        right: 30.0,
+                        top: 10.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "LOCAL EVENTS",
+                            style: fadedTextStyle,
+                          ),
+                          Spacer(),
+                          Icon(
+                            Icons.person_outline,
+                            color: Color(0x99FFFFFF),
+                            size: 30.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 30.0),
+                      child: Text(
+                        "What's Up",
+                        style: whiteHeadingTextStyle,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Consumer(
+                        builder: (context, appState, _) =>
+                            SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              for (final category in categories)
+                                CategoryWidget(category: category)
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
